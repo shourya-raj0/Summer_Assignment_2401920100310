d6554fa2ea79d8cb263d43bddec77e98f2a53e2a
shourya <shourya4536@gmail.com>
2026-06-02 22:24:47 +0530

    Update Maximum Subarray.cpp

--- Week_1/Day_2/Maximum Subarray.cpp
+++ Week_1/Day_2/Maximum Subarray.cpp
@@ -1,1 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum=nums[0];
+        int sum=nums[0];
 
+        for(int i=1; i< nums.size();i++){
+            sum = max(nums[i],nums[i]+sum);
+            maxsum= max(maxsum,sum);
+                       
+        }
+        return maxsum;
+    }
+};
