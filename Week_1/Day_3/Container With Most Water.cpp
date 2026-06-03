9a8b73542c187caad8267cc4fd72fcf08336d03d
shourya <shourya4536@gmail.com>
2026-06-03 13:17:23 +0530

    Update Container With Most Water.cpp

--- Week_1/Day_3/Container With Most Water.cpp
+++ Week_1/Day_3/Container With Most Water.cpp
@@ -1,1 +1,24 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxarea= 0;
+int l=0;
+int r= height.size()-1;
 
+while(l<r){
+    int h= min(height[l],height[r]);
+    int area = h* (r-l);
+
+    maxarea= max(area,maxarea);
+
+    if(height[l]<height[r]){
+        l++;
+    }
+    else{
+        r--;
+    }
+}
+return maxarea;
+
+    }
+};
