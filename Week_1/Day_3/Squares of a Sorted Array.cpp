94bf34002e17002ce07405da2ce654513ac72a29
shourya <shourya4536@gmail.com>
2026-06-03 11:15:24 +0530

    Update Squares of a Sorted Array.cpp

--- Week_1/Day_3/Squares of a Sorted Array.cpp
+++ Week_1/Day_3/Squares of a Sorted Array.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
 
+        int i = 0, j = n - 1, k = n - 1;
+
+        while (i <= j) {
+            int sq_i = nums[i] * nums[i];
+            int sq_j = nums[j] * nums[j];
+
+            if (sq_i > sq_j) {
+                ans[k--] = sq_i;
+                i++;
+                
+            } 
+             else {
+                ans[k--]=sq_j;
+                j--;
+            }
+        }
+        return ans;
+    }
+};
