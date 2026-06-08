dddf63630e5e28eb5d19a93515f9b17c7fda3cae
shourya <shourya4536@gmail.com>
2026-06-08 22:07:02 +0530

    Create First Unique Character in a string.cpp

---
+++ Week_2/Day_1/First Unique Character in a string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        vector<int> count(26, 0);
+
+        for(char i:s){
+            count[i-'a']++;
+        }
+
+        for(int i=0 ;i<s.length();i++){
+            if(count[s[i]-'a']==1)
+            return i;
+        }
+        return -1;
+    }
+};
