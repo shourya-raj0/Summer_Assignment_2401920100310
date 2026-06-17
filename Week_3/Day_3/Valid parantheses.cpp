32d01fd991e0ece2df46671f48668af99e0e4e51
shourya <shourya4536@gmail.com>
2026-06-17 13:41:18 +0530

    Create Valid parantheses.cpp

---
+++ Week_3/Day_3/Valid parantheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+
+        for(char ch:s){
+            if(ch=='(' || ch=='[' || ch=='{')
+            st.push(ch);
+
+            else {
+
+                if (!st.empty()) {
+                    char top = st.top();
+
+                    if ((top == '(' && ch == ')') ||
+                        (top == '{' && ch == '}') || (top == '[' && ch == ']'))
+                        st.pop();
+
+                    else
+                      return false;
+                }
+                else
+                return false;
+            }
+        }
+        return st.empty();
+    }
+};
